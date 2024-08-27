00ac3328bc37f6a1b1e9dde7b7da0d34f90bc2d9
Alexandre Luchetti <luchetti.92@gmail.com>
2024-08-26 21:24:00 -0300

    First commit

---
+++ src/main/java/br/com/mobiauto/mobiauto_server/MobiautoServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mobiauto.mobiauto_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiautoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiautoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mobiauto/mobiauto_server/MobiautoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mobiauto.mobiauto_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiautoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
